e65cde01b17de74a944951ec26e06a0236a55981
Spencer Tiberi <spencertiberi@gmail.com>
2019-06-10 11:54:19 -0400

    Add MuiTheme provider

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
 import AlertTemplate from 'react-alert-template-basic'
 import localStore from 'store'
 import theme from './theme'
@@ -28,6 +29,17 @@ import HeaderBar from './components/HeaderBar'
 import EmailRecoveryScreen from './containers/EmailRecoveryScreen'
 import VerificationCodeScreen from './containers/VerificationCodeScreen'
 import NewPasswordScreen from './containers/NewPasswordScreen'
+
+const MuiTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: theme.colors.tertiary
+    },
+    secondary: {
+      main: theme.colors.secondary
+    }
+  }
+})
 
 const options = {
   position: positions.BOTTOM_CENTER,
@@ -63,49 +75,59 @@ class App extends React.Component {
         <PersistGate loading={null} persistor={persistor}>
           <Router>
             <ThemeProvider theme={theme}>
-              <ApolloProvider client={client}>
-                <AlertProvider template={AlertTemplate} {...options}>
-                  <div className="App">
-                    <HeaderBar />
-                    <Switch>
-                      <PrivateRoute
-                        exact
-                        path="/"
-                        component={isLoggedIn ? RequestContributeScreen : Home}
-                      />
-                      <Route path="/login" component={LoginScreen} />
-                      <Route path="/register" component={WorkEmailScreen} />
-                      <Route path="/register-cont" component={PasswordScreen} />
-                      <Route path="/details" component={UserDetailsScreen} />
-                      <Route
-                        path="/bank-info"
-                        component={EnterBankInfoScreen}
-                      />
-                      <Route
-                        path="/complete-profile"
-                        component={CompleteProfileScreen}
-                      />
-                      <Route path="/recovery" component={EmailRecoveryScreen} />
-                      <Route
-                        path="/verification"
-                        component={VerificationCodeScreen}
-                      />
-                      <Route
-                        path="/new-password"
-                        component={NewPasswordScreen}
-                      />
-                      <PrivateRoute
-                        path="/profile"
-                        component={EditProfileScreen}
-                      />
-                      <PrivateRoute
-                        path="/request"
-                        component={RequestContributeScreen}
-                      />
-                    </Switch>
-                  </div>
-                </AlertProvider>
-              </ApolloProvider>
+              <MuiThemeProvider theme={MuiTheme}>
+                <ApolloProvider client={client}>
+                  <AlertProvider template={AlertTemplate} {...options}>
+                    <div className="App">
+                      <HeaderBar />
+                      <Switch>
+                        <PrivateRoute
+                          exact
+                          path="/"
+                          component={
+                            isLoggedIn ? RequestContributeScreen : Home
+                          }
+                        />
+                        <Route path="/login" component={LoginScreen} />
+                        <Route path="/register" component={WorkEmailScreen} />
+                        <Route
+                          path="/register-cont"
+                          component={PasswordScreen}
+                        />
+                        <Route path="/details" component={UserDetailsScreen} />
+                        <Route
+                          path="/bank-info"
+                          component={EnterBankInfoScreen}
+                        />
+                        <Route
+                          path="/complete-profile"
+                          component={CompleteProfileScreen}
+                        />
+                        <Route
+                          path="/recovery"
+                          component={EmailRecoveryScreen}
+                        />
+                        <Route
+                          path="/verification"
+                          component={VerificationCodeScreen}
+                        />
+                        <Route
+                          path="/new-password"
+                          component={NewPasswordScreen}
+                        />
+                        <PrivateRoute
+                          path="/profile"
+                          component={EditProfileScreen}
+                        />
+                        <PrivateRoute
+                          path="/request"
+                          component={RequestContributeScreen}
+                        />
+                      </Switch>
+                    </div>
+                  </AlertProvider>
+                </ApolloProvider>
+              </MuiThemeProvider>
             </ThemeProvider>
           </Router>
         </PersistGate>

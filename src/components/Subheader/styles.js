848934c4a5cb25fcaccc848261a2ddb039289667
chancyj <clarence.chan@mail.hsa.net>
2019-06-24 15:27:26 -0400

    Styling(Roboto Font) -- Returned all fonts to roboto

--- src/components/Subheader/styles.js
+++ src/components/Subheader/styles.js
@@ -4,7 +4,7 @@ import { Flex } from 'grid-styled'
 export const StyledSubheader = styled(Flex)`
   color: ${({ color, theme }) => color || theme.colors.tertiary};
   font-size: 18px;
-  font-family: ${({ theme }) => theme.fonts.medium};
+  font-family: ${({ theme }) => theme.fonts.medium.family};
   align-self: ${({ alignSelf }) => alignSelf || 'flex-start'};
   margin-top: 10px;
   margin-bottom: 10px;
--- src/containers/RequestContributeScreen/components/RequestScreen/styles.js
+++ src/containers/RequestContributeScreen/components/RequestScreen/styles.js
@@ -4,7 +4,7 @@ import { Flex } from 'grid-styled'
 export const Span = styled(Flex)`
   color: ${({ theme }) => theme.colors.tertiary};
   font-size: 24px;
-  font-family: ${({ theme }) => theme.fonts.medium};
+  font-family: ${({ theme }) => theme.fonts.medium.family};
   flex-direction: column;
   justify-content: center;
   padding-right: 10px;

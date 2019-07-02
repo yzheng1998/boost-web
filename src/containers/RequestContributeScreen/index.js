2c140553c14d434611e1bb2779b6e98b5c89171b
chancyj <clarence.chan@mail.hsa.net>
2019-07-02 10:20:15 -0400

    Styling(RequestcontributeScreen Abstraction) -- Abstracted query into SFC

--- src/containers/RequestContributeScreen/index.js
+++ src/containers/RequestContributeScreen/index.js
@@ -1,44 +1,27 @@
 import React from 'react'
-import { Query } from 'react-apollo'
 import { withStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
 import Background from '../../components/Background'
-import RequestScreen from './components/RequestScreen'
-import ContributeScreen from './components/ContributeScreen'
-import LoadingIcon from '../../components/LoadingIcon'
 import { styles } from './styles'
 import theme from '../../theme'
-import { GET_USER } from './graphql'
+import CurrentScreen from './components/CurrentScreen'
+
+const LOADING = 2
 
 class CenteredTabs extends React.Component {
   state = {
-    contributions: 0,
-    requests: 0,
-    payPalEmail: '',
-    value: 0
+    tab: LOADING
   }
 
-  componentDidMount = () => {
-    this.setState({ value: 2 })
+  onChange = (event, tab) => {
+    this.setState({ tab })
   }
-
-  onChange = (event, value) => {
-    this.setState({ value })
-  }
-
-  handleError = ({ message }) => {
-    console.error(message)
-    return 'An error occurred!'
-  }
-
-  computeBalance = ({ requests, contributions }) =>
-    (1000 - requests + contributions).toFixed(2)
 
   render() {
     const { classes } = this.props
-    const { value } = this.state
+    const { tab } = this.state
 
     return (
       <Background
@@ -49,7 +32,7 @@ class CenteredTabs extends React.Component {
       >
         <Paper className={classes.root}>
           <Tabs
-            value={value}
+            value={tab}
             onChange={this.onChange}
             textColor="primary"
             indicatorColor="primary"
@@ -59,51 +42,11 @@ class CenteredTabs extends React.Component {
             <Tab label="Contribute" />
           </Tabs>
         </Paper>
-        <Query
-          query={GET_USER}
-          onCompleted={data => {
-            this.setState({
-              contributions: data.viewer.user.contributions,
-              requests: data.viewer.user.requests,
-              payPalEmail: data.viewer.user.personalEmail
-            })
-          }}
-        >
-          {({ loading, data, error }) => {
-            if (loading) return <LoadingIcon />
-            if (error) {
-              this.handleError(error)
-            }
-            const { user } = data.viewer
-
-            const balance = this.computeBalance(user)
-
-            if (value === 0) {
-              return (
-                <RequestScreen
-                  data={this.state}
-                  balance={balance}
-                  history={this.props.history}
-                />
-              )
-            } else if (value === 1) {
-              return <ContributeScreen />
-            }
-
-            if (balance === '1000.00') {
-              this.setState({ value: 0 })
-              return (
-                <RequestScreen
-                  data={this.state}
-                  balance={balance}
-                  history={this.props.history}
-                />
-              )
-            }
-            this.setState({ value: 1 })
-            return <ContributeScreen />
-          }}
-        </Query>
+        <CurrentScreen
+          setState={p => this.setState(p)}
+          tab={this.state.tab}
+          history={this.props.history}
+        />
       </Background>
     )
   }

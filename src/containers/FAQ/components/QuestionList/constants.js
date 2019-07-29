088db40c34f8da8a873ff4aa90900ee87838a096
chancyj <clarence.chan@mail.hsa.net>
2019-07-29 12:26:04 -0400

    Styling(QuestionList) -- Created question list component

---
+++ src/containers/FAQ/components/QuestionList/constants.js
@@ -0,0 +1,8 @@
+export const Questions = [
+  { id: 1, question: 'What is GreenPath Grant Circle?', answer: 'It is a ...' },
+  {
+    id: 2,
+    question: 'How much can I request?',
+    answer: 'You can withdraw a maximum of $1000'
+  }
+]

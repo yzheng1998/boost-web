0d6400fbc62cea42de469a6a00d0084a1c7065a7
chancyj <clarence.chan@mail.hsa.net>
2019-08-01 17:34:41 -0400

    Styling(RequestSubmittedModal) -- Created request submitted modal

---
+++ src/containers/RequestContributeScreen/components/CurrentScreen/components/RequestScreen/components/RequestSubmittedModal/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import ModalContainer from './styles'
+import { Button } from '@material-ui/core'
+
+const RequestSubmittedModal = ({ handleSuccess }) => (
+  <ModalContainer>
+    <DialogTitle>Your request has been submitted!</DialogTitle>
+    <DialogContent>
+      <DialogContentText>
+        Congratulations, your request has been successfully! Our admins are
+        working to approve your request as soon as possible. Please allow up to
+        3-5 business days to hear a response and thank you again for choosing
+        GreenPath Grant Circle.
+      </DialogContentText>
+      <DialogActions>
+        <Button onClick={() => handleSuccess()}>Ok</Button>
+      </DialogActions>
+    </DialogContent>
+  </ModalContainer>
+)
+
+export default RequestSubmittedModal

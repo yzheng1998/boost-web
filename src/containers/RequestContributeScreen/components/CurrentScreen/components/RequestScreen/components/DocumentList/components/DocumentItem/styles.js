a3509edc3234afcaf9d3fbedf63d05ab56336f2d
chancyj <clarence.chan@mail.hsa.net>
2019-08-01 15:02:13 -0400

    Styling(Document Item) -- Created document item component

---
+++ src/containers/RequestContributeScreen/components/CurrentScreen/components/RequestScreen/components/DocumentList/components/DocumentItem/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+import { Flex } from '@rebass/grid'
+
+export const ItemContainer = styled(Flex)`
+  border: 1px solid grey;
+  justify-content: space-between;
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  padding-left: 5px;
+  padding-right: 5px;
+  margin-bottom: 8px;
+`
+
+export const DocumentName = styled(Flex)``

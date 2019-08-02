ce96bc45cc2ce6c26e814970ea869671caa01228
chancyj <clarence.chan@mail.hsa.net>
2019-08-02 11:26:40 -0400

    Styling(Link FAQ) -- Linked modal to faq screen

--- src/containers/RequestContributeScreen/components/CurrentScreen/components/RequestScreen/components/RequestSubmittedModal/index.js
+++ src/containers/RequestContributeScreen/components/CurrentScreen/components/RequestScreen/components/RequestSubmittedModal/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogContentText from '@material-ui/core/DialogContentText'
@@ -11,10 +12,11 @@ const RequestSubmittedModal = ({ handleSuccess }) => (
     <DialogTitle>Your request has been submitted!</DialogTitle>
     <DialogContent>
       <DialogContentText>
-        Congratulations, your request has been successfully! Our admins are
-        working to approve your request as soon as possible. Please allow up to
-        3-5 business days to hear a response and thank you again for choosing
-        GreenPath Grant Circle.
+        Congratulations, your request has been successfully submitted! Our
+        admins are working to approve your request as soon as possible. Please
+        allow up to 3-5 business days to hear a response and thank you again for
+        choosing GreenPath Grant Circle. For more information, please see{' '}
+        <Link to="/faq">FAQ</Link>
       </DialogContentText>
       <DialogActions>
         <Button onClick={() => handleSuccess()}>Ok</Button>

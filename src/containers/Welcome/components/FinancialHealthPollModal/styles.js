0f9f47fa748aa46bf493e42b015895a1c3e5dcd4
chancyj <clarence.chan@mail.hsa.net>
2019-08-06 13:22:17 -0400

    Styling(FinancialHealthModal) -- Created financial health poll modal

---
+++ src/containers/Welcome/components/FinancialHealthPollModal/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import { Flex } from '@rebass/grid'
+
+const ModalContainer = styled(Flex)`
+  flex-direction: column;
+  width: 500px;
+`
+
+export default ModalContainer

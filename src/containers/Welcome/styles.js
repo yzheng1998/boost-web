88d9f8f7d3b06fd7cbd54722869471ccebc8eb1d
chancyj <clarence.chan@mail.hsa.net>
2019-08-08 10:34:59 -0400

    Styling(Welcome Screen) -- Optimized welcome screen for mobile

--- src/containers/Welcome/styles.js
+++ src/containers/Welcome/styles.js
@@ -5,6 +5,7 @@ export const WelcomeScreen = styled(Flex)`
   height: auto;
   min-height: 100vh;
   flex-direction: column;
+  margin-top: 20px;
 `
 
 export const InfoText = styled(Flex)`
@@ -16,4 +17,9 @@ export const ButtonsContainer = styled(Flex)`
   align-self: center;
   justify-content: space-around;
   width: 425px;
+  @media screen and (max-width: ${({ theme }) =>
+      theme.breakpoints.mobile.ceiling}) {
+    flex-direction: column;
+    justify-content: flex-start;
+  }
 `

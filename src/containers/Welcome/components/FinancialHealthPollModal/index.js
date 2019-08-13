26e5807b9c1e1120b8438192d9a22a64a4fdc1b2
docovarr2020 <diego_covarrubias@college.harvard.edu>
2019-08-13 11:00:32 -0400

    Styling(FinancialHealthPoll Radio Buttons) -- Replace the slider with the HealthOptions component in the Financial Health Poll Modal

--- src/containers/Welcome/components/FinancialHealthPollModal/index.js
+++ src/containers/Welcome/components/FinancialHealthPollModal/index.js
@@ -1,34 +1,26 @@
 import React, { useState } from 'react'
-import Slider from '@material-ui/core/Slider'
 import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import ModalContainer from './styles'
-import Labels from './components/Labels'
 import ConfirmButton from './components/ConfirmButton'
+import HealthOptions from './components/HealthOptions'
 
 const FinancialHealthPollModal = ({ setOpen }) => {
-  const [financialHealthRating, setFinancialHealthRating] = useState(50)
-
+  const [financialHealthRating, setFinancialHealthRating] = useState('50')
   return (
     <ModalContainer>
       <DialogTitle>How is your financial health?</DialogTitle>
       <DialogContent
         style={{
-          height: 150,
           display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
           flexDirection: 'column'
         }}
       >
-        <Slider
-          getAriaValueText={val => setFinancialHealthRating(val)}
-          defaultValue={50}
-          step={1}
-          valueLabelDisplay="auto"
+        <HealthOptions
+          onChange={setFinancialHealthRating}
+          currentValue={financialHealthRating}
         />
-        <Labels />
       </DialogContent>
       <DialogActions>
         <ConfirmButton

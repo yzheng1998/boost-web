b73e686a9203e49c837537bb633b5738535e882b
josephokeefe <josephokeefe@college.harvard.edu>
2019-08-19 16:38:09 -0400

    Styling(HowItWorks)

---
+++ src/containers/HowItWorksScreen/components/ScreenSelector/components/HowItWorks3/styles.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components'
+import { Flex } from 'grid-styled'
+import CenterParagraph from '../CenterParagraph'
+
+export const Paragraph = styled(CenterParagraph)`
+  @media screen and (max-width: ${({ theme }) =>
+      theme.breakpoints.mobile.ceiling}) {
+    min-height: 70px;
+  }
+`
+
+export const BtnRow = styled(Flex)`
+  justify-content: center;
+  flex-direction: row;
+`
+
+export const Label = styled(Flex)`
+  font-family: ${({ theme }) => theme.fonts.medium.family};
+  font-size: 14px;
+  margin-bottom: 12px;
+  color: ${({ theme }) => theme.colors.black};
+  align-self: center;
+  text-align: center;
+`
+
+export const ImgContainer = styled.img`
+  width: 100px;
+  height: 100px;
+  margin: 30px;
+  margin-bottom: 40px;
+`
+
+export const NumbersContainer = styled(Flex)`
+  width: 100%;
+  height: 100px;
+  margin-top: 60px;
+  align-items: center;
+  justify-content: center;
+  align-self: center;
+  @media screen and (max-width: ${({ theme }) =>
+      theme.breakpoints.mobile.ceiling}) {
+    width: 100%;
+  }
+`
+
+export const Container = styled(Flex)`
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  align-self: center;
+  @media screen and (max-width: ${({ theme }) =>
+      theme.breakpoints.mobile.ceiling}) {
+    width: 100%;
+  }
+`

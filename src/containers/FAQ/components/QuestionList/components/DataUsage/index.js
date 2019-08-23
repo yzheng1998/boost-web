36d190f3f726f0fbc7a025dab8fe576a4c4eebdf
chancyj <clarence.chan@mail.hsa.net>
2019-08-23 13:21:05 -0400

    Styling(DataUsage) -- Created data usage component

---
+++ src/containers/FAQ/components/QuestionList/components/DataUsage/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Typography } from '@material-ui/core'
+
+const DataUsage = () => (
+  <Typography>
+    All data collected through the Grant Circles web application, surveys, and
+    from your employer, are used for research purposes only. We also use some
+    data, such as the information and documentation submitted with fund
+    requests, to evaluate whether and how much to disburse to pilot
+    participants. We will never sell your data or share it in an individually
+    identifying manner. We will leverage learnings from the pilot to improve the
+    Grant Circles offering, and will share aggregate learnings with our funders
+    and partners. Additional detail about data use can be found in the Terms and
+    Conditions.
+  </Typography>
+)
+
+export default DataUsage

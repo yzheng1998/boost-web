383d87d9bdf7af8cf108fda1974af92c077cc7ab
chancyj <clarence.chan@mail.hsa.net>
2019-08-23 13:22:18 -0400

    Styling(Required Info And Docs) -- Created required info and docs component

---
+++ src/containers/FAQ/components/QuestionList/components/RequiredInformationAndDocumentation/constants.js
@@ -0,0 +1,5 @@
+export const AcceptedDocuments = [
+  'Bills or receipts showing expenses related to the hardship and need for funds.',
+  'Documents showing changes in income or expenses due (paystubs, late notices, etc.)',
+  'Any other documents that provide supporting detail about the financial hardship and need for funds (examples: bank statements, eviction warnings, etc)'
+]

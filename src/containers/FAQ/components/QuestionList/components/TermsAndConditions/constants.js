880b9e999ca6995a1417b17b43559d867d3e05bc
chancyj <clarence.chan@mail.hsa.net>
2019-08-29 14:43:04 -0400

    Styling(Terms And Conditions) -- Created terms and conditions

---
+++ src/containers/FAQ/components/QuestionList/components/TermsAndConditions/constants.js
@@ -0,0 +1,7 @@
+export const consentList = [
+  'Being contacted by Capital Access Strategies to participate in follow-up surveys and/or interviews related to their experience in the pilot',
+  'Their employer sharing individualized workplace data with Capital Access Strategies for research purposes (to join with other data at an individual level)',
+  'Capital Access Strategies analyzing data collected from the Grant Circle application, surveys, interviews, and employer workplace data to understand the financial health and workplace impact of hardship grants and to improve its Grant Circle solution.',
+  'Capital Access Strategies sharing aggregate, summarized data and learnings with their employer (individually identifiable use data and survey responses will not be shared with the employer)',
+  'Capital Access Strategies discussing the pilot and summarized learnings from the pilot publicly (again, individually identifiable use data and survey responses will not be shared publicly)'
+]

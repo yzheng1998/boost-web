528eea21788f6aeaf636a3d459c0f3029cd88c3a
josephokeefe <josephokeefe@college.harvard.edu>
2019-09-02 08:58:21 -0400

    BugFix(Request) -- event description text area

--- src/containers/RequestContributeScreen/components/CurrentScreen/components/RequestScreen/components/EventExplanationBlock/index.js
+++ src/containers/RequestContributeScreen/components/CurrentScreen/components/RequestScreen/components/EventExplanationBlock/index.js
@@ -12,7 +12,7 @@ const EventExplanationBlock = ({
     <Description
       headerText="Please provide a brief description of the situation, including approximate dates for any of the events you selected above and how they created the need for funds:"
       labelText="Enter Description"
-      name="hardshipExplanation"
+      name="eventsExplanation"
       value={eventsExplanation}
       onChange={onChange}
       addTouched={addTouched}

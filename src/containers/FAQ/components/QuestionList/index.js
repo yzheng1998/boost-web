ad7355d286928b8bb54ed464dcd5fcdb5baa5829
josephokeefe <josephokeefe@college.harvard.edu>
2019-09-02 09:32:08 -0400

    Feature(FAQ) -- google analytics

--- src/containers/FAQ/components/QuestionList/index.js
+++ src/containers/FAQ/components/QuestionList/index.js
@@ -4,16 +4,31 @@ import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
 import Typography from '@material-ui/core/Typography'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import ReactGA from 'react-ga'
+import config from '../../../../config'
 import { Questions } from './constants'
+
+ReactGA.initialize(config.gaTrackingCode)
 
 const QuestionList = () => {
   const [expanded, setExpanded] = useState(false)
 
-  const handleChange = panel => (event, isExpanded) => {
-    setExpanded(isExpanded ? panel : false)
+  const handleChange = (panel, question) => (event, isExpanded) => {
+    if (isExpanded) {
+      setExpanded(panel)
+      ReactGA.event({
+        category: 'FAQ',
+        action: question
+      })
+    } else {
+      setExpanded(false)
+    }
   }
   return Questions.map(({ id, question, answer, component }) => (
-    <ExpansionPanel expanded={expanded === id} onChange={handleChange(id)}>
+    <ExpansionPanel
+      expanded={expanded === id}
+      onChange={handleChange(id, question)}
+    >
       <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
         <Typography style={{ fontStyle: 'normal', fontWeight: 'bold' }}>
           {question}

6f3949ef88fb80237bed4a3e02dbd88ce2353f9d
chancyj <clarence.chan@mail.hsa.net>
2019-09-05 01:37:29 -0400

    Styling(Required Text) -- Added link to FAQ on required text

--- src/containers/RequestContributeScreen/components/CurrentScreen/components/RequestScreen/components/DocumentBlock/index.js
+++ src/containers/RequestContributeScreen/components/CurrentScreen/components/RequestScreen/components/DocumentBlock/index.js
@@ -1,14 +1,29 @@
+/* eslint-disable react/jsx-indent */
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Subheader from '../../../../../../../../components/Subheader'
 import Row from '../../../../../../../../components/Row'
 import DocumentInput from '../DocumentInput'
 import DocumentList from '../DocumentList'
 import InformationText from './components/InformationText'
 
+const RequiredText = () => (
+  <div>
+    Documentation of the financial hardship is REQUIRED for this request. For
+    more detail on eligible fund uses and documentation requirements, please
+    refer to the&nbsp;
+    <Link to="/faq" target="_blank" rel="noopener noreferrer">
+      FAQ.
+    </Link>
+  </div>
+)
+
 const DocumentTitle = ({ fundsWithdrawn }) =>
-  fundsWithdrawn > 400
-    ? 'Documentation of the financial hardship is REQUIRED for this request. For more detail on eligible fund uses and documentation requirements, please refer to the FAQ.'
-    : 'Documentation of the financial hardship is OPTIONAL for this request'
+  fundsWithdrawn > 400 ? (
+    <RequiredText />
+  ) : (
+    'Documentation of the financial hardship is OPTIONAL for this request'
+  )
 
 const DocumentBlock = ({
   fundsWithdrawn,
